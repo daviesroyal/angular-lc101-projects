be1a1e1ab2952cb196a55ec01ec594bab241c04c
Alex Davies <ro.morningstar@gmail.com>
2020-11-03 16:46:53 -0600

    completed

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,8 +25,16 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
+  addToCrew(candidate: object) {
+    if (!this.crew.includes(candidate)) {
+      this.crew.push(candidate);
+    }
+  }
 
   // BONUS: Code the changeMissionName function here:
 
+  changeMissionName(newName: string) {
+    this.missionName = newName;
+  }
 
 }
